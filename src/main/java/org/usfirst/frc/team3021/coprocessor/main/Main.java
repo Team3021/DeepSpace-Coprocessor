73414f32b63396a648c8ef36e866e087699715c8
Neightro <nathan@codiem.com>
2019-02-15 21:22:07 -0800

    Remove unused imports, init method, and add logic to keep the main thread alive

--- src/main/java/org/usfirst/frc/team3021/coprocessor/main/Main.java
+++ src/main/java/org/usfirst/frc/team3021/coprocessor/main/Main.java
@@ -1,30 +1,24 @@
 package org.usfirst.frc.team3021.coprocessor.main;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.core.MatOfPoint;
-import org.opencv.core.Point;
-import org.opencv.core.RotatedRect;
 import org.usfirst.frc.team3021.coprocessor.camera.VisionSystem;
-import org.usfirst.frc.team3021.coprocessor.processing.Drawing;
-import org.usfirst.frc.team3021.coprocessor.processing.Filtering;
-import org.usfirst.frc.team3021.coprocessor.processing.Targeting;
-import org.usfirst.frc.team3021.coprocessor.target.HatchTarget;
 
 public class Main {
 	private static VisionSystem visionSystem;
 	
-	public static void init() {
-		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-	}
 	
 	public static void main(String[] args) {
-		init();
+		
+		System.out.println("Starting Vision System");
 		
 		visionSystem = new VisionSystem();
+		
+		for (;;) {
+			try {
+				Thread.sleep(10000);
+			} catch (InterruptedException e) {
+				return;
+			}
+		}
 	}
 	
 }
